e81269e817fa9a55418026c5441c40fc23036b3f
RazorBlade22 <95306816+RazorBlade22@users.noreply.github.com>
2023-12-10 11:41:38 -0500

    Initial commit

---
+++ Assets/TemplateScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+
+public class TemplateScript : MonoBehaviour {
+
+    static int _moduleIdCounter = 1;
+    int _moduleID = 0;
+
+    public KMBombModule Module;
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+
+    void Awake()
+    {
+        _moduleID = _moduleIdCounter++;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
